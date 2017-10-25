d7c603d4d00515a7c25c6aea8300a45dbd558c11
Vitor Andriotti <vandriotti@alertlogic.com>
2017-10-24 22:14:18 -0200

    Add better comments

--- line_follower_pid/line_follower_pid.ino
+++ line_follower_pid/line_follower_pid.ino
@@ -1,33 +1,82 @@
 #include <AFMotor.h>
- 
-AF_DCMotor motor_esq(1); //Seleciona o motor 1
-AF_DCMotor motor_dir(4); //Seleciona o motor 4
 
-#define Kp 0.3 // 0.06 // 0.3
-#define Kd 4.5 // 0.075 Kp < Kd
-#define Ki 3.1 // TBD
-#define rightMaxSpeed 96//96 // max speed
-#define leftMaxSpeed 70//70 // max speed
-#define rightBaseSpeed 66 // straight line speed
-#define leftBaseSpeed 40  // straight line speed
+// ----------------------- Constantes
 
+AF_DCMotor motor_esq(1);       // Seleciona o motor 1
+AF_DCMotor motor_dir(4);       // Seleciona o motor 4
+
+#define Kp 0.3                 // 0.06 // 0.3
+#define Kd 4.5                 // 0.075 Kp < Kd
+#define Ki 3.1                 //
+#define rightMaxSpeed 96//96   // Velocidade máxima (roda direita)
+#define leftMaxSpeed 70//70    // Velocidade máxima (roda esquerda)
+#define rightBaseSpeed 66      // Velocidade para andar em linha reta (roda direita)
+#define leftBaseSpeed 40       // Velocidade para andar em linha reta (roda esquerda)
+
+// ----------------------- Inicialização de variáveis globais
 int SENSOR1, SENSOR2, SENSOR3;
- 
-//deslocamentos de calibracao
-int leftOffset = 0, rightOffset = 0, centre = 0;
-//limiar do sensor
-int threshold = 200;
 
-int lastError = 0;
-int I = 0;
+int leftOffset = 0, rightOffset = 0, centre = 0; // Deslocamentos de calibração
+int threshold = 200;                             // Limiar do sensor
+int lastError = 0;                               // Último erro
+int I = 0;                                       // Inicializa I
+unsigned long time;                              // Variável para controlar o tempo corrente
+unsigned long lastTime = 0;                      // Inicializa variável que armazena o tempo da última leitura
 
-unsigned long time;
-unsigned long lastTime = 0;
+// ----------------------- SETUP
+void setup()
+{
+  calibrate(); // Executa rotina de calibração
+  delay(3000);
+  Serial.begin(9600);
+  send_speed_to_motor(rightMaxSpeed, leftMaxSpeed);
+  delay(100);
+}
+
+// ----------------------- MAIN LOOP
+void loop()
+{
+  // Lê os sensores e adiciona os deslocamentos (offsets)
+  SENSOR1 = analogRead(0) + leftOffset;
+  SENSOR2 = analogRead(1);
+  SENSOR3 = analogRead(2) + rightOffset;
+
+  int position = convert_sensor_to_position();        // Lê posição atual (1000 = centro da fita)
+  
+  int error = position - 1000;                        // Subtrai 1000 do erro para que o mesmo seja deslocado para 0
+
+  time = millis();                                    // millis() retorna o temp atual em uma timestamp (epoch)
+  int E = error - lastError;                          // Calcula diferença entre erro atual e anterior
+  if(lastTime == 0) lastTime = time;                  // Se lastTime = 0 significa que não houve uma leitura ainda, logo, ultimo_tempo = tempo_atual
+  int deltaTime = time - lastTime;                    // Calcula variação de tempo (delta)
+  lastTime = time;                                    // Armazena tempo atual em lastTime para ser utilizado no próximo ciclo
+
+  int P = error;                                      // Componente Proporcional
+  int I = I + (E*deltaTime);//I + error;              // Componente Integral
+  int D = (error - lastError);                        // Componente Derivativa
+  int motorSpeed = (Kp * P) + (Kd * D) + (Ki * I);    // Velocidade do motor = P + I + D
+  lastError = error;                                  // Armazena o erro para ser utilizado como erro anterior no próximo ciclo
+  
+  int rightMotorSpeed = rightBaseSpeed + motorSpeed;  // Velocidade do motor direito = velocidade base + velocidade cálculada
+  int leftMotorSpeed = leftBaseSpeed - motorSpeed;    // Velocidade do motor esquero = velocidade base - velocidade cálculada
+
+  // Condicionais para impedir que os motores passem das velocidades máximas definidas e também não recebam velocidades < 0
+  if (rightMotorSpeed > rightMaxSpeed ) rightMotorSpeed = rightMaxSpeed;
+  if (leftMotorSpeed > leftMaxSpeed ) leftMotorSpeed = leftMaxSpeed;
+  if (rightMotorSpeed < 0) rightMotorSpeed = 0;
+  if (leftMotorSpeed < 0) leftMotorSpeed = 0;
  
+  // Envia os valores de velocidade para os motores
+  //print_motor_speed(rightMotorSpeed, leftMotorSpeed);
+  send_speed_to_motor(rightMotorSpeed, leftMotorSpeed);
+  //delay(5);
+}
+
 //Rotina de calibracao do sensor
 void calibrate()
 {
- for (int x=0; x<10; x++) //Executa 10 vezes para obter uma media
+ // Rotina executada 10 vezes para se obter uma média
+ for (int x=0; x<10; x++)
  {
    delay(100);
    SENSOR1 = analogRead(0);
@@ -38,61 +87,16 @@ void calibrate()
    rightOffset = rightOffset + SENSOR3;
    delay(100);
  }
- //obtem a media para cada sensor
+ // Calcula a média de cada sensor
  leftOffset = leftOffset /10;
  rightOffset = rightOffset /10;
  centre = centre / 10;
- //calcula os deslocamentos para os sensores esquerdo e direito
+ // Calcula os deslocamentos para os sensores esquerdo e direito a partir do sensor central
  leftOffset = centre - leftOffset;
  rightOffset = centre - rightOffset;
 }
- 
-void setup()
-{
-  calibrate();
-  delay(3000);
-  Serial.begin(9600);
-  send_speed_to_motor(rightMaxSpeed, leftMaxSpeed);
-  delay(100);
-}
 
-void loop()
-{
-  //le os sensores e adiciona os deslocamentos
-  SENSOR1 = analogRead(0) + leftOffset;
-  SENSOR2 = analogRead(1);
-  SENSOR3 = analogRead(2) + rightOffset;
-
-  int position = convert_sensor_to_position();
-  
-  int error = position - 1000;
-
-  time = millis();
-  int E = error - lastError;
-  if(lastTime == 0) lastTime = time;
-  int deltaTime = time - lastTime;
-  lastTime = time;
-
-  int P = error;
-  int I = I + (E*deltaTime);//I + error;
-  int D = (error - lastError);
-  int motorSpeed = (Kp * P) + (Kd * D) + (Ki * I);
-  lastError = error;
-  
-  int rightMotorSpeed = rightBaseSpeed + motorSpeed;
-  int leftMotorSpeed = leftBaseSpeed - motorSpeed;
-
-  if (rightMotorSpeed > rightMaxSpeed ) rightMotorSpeed = rightMaxSpeed;
-  if (leftMotorSpeed > leftMaxSpeed ) leftMotorSpeed = leftMaxSpeed;
-  if (rightMotorSpeed < 0) rightMotorSpeed = 0;
-  if (leftMotorSpeed < 0) leftMotorSpeed = 0;
- 
-  //Envia os valores de velocidade para os motores
-  //print_motor_speed(rightMotorSpeed, leftMotorSpeed);
-  send_speed_to_motor(rightMotorSpeed, leftMotorSpeed);
-  //delay(5);
-}
-
+// Função que converte a leitura dos sensores (0 - 1023) para um valor de posição ~(0-1000)
 unsigned long convert_sensor_to_position(){
   //print_sensors_readings();
   // normalizar valores;
@@ -105,6 +109,7 @@ unsigned long convert_sensor_to_position(){
   return position;
 }
 
+// Função auxiliar para debugar a leitura dos sensores através do monitor serial
 void print_sensors_readings(){
   Serial.print("Sensor 1 ");
   Serial.print(SENSOR1);
@@ -114,6 +119,7 @@ void print_sensors_readings(){
   Serial.println(SENSOR3);
 }
 
+// Função auxiliar para debugar velocidade dos motores através do monitor serial
 void print_motor_speed(int rightMotorSpeed, int leftMotorSpeed){
   Serial.print("Right motor speed: ");
   Serial.print(rightMotorSpeed);
